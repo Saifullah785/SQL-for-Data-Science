dc80a8cd53b09ee6f8a67b319bc37a48743bc835
Saif Ullah <muhammadalisaif785@gmail.com>
2025-07-20 08:24:14 +0500

    Created Using MySQL Workbench
    while working on Aggregate Function with over() in window function in sql

--- Lecture_07_Window_Functions_in_SQL/window_function_in_sql_queries.sql
+++ Lecture_07_Window_Functions_in_SQL/window_function_in_sql_queries.sql
@@ -24,3 +24,41 @@ INSERT INTO marks (name,branch,marks)VALUES
 ('Amit','MECH',69),
 ('Sunny','MECH',39),
 ('Gautam','MECH',51)
+
+SELECT *, AVG(marks) OVER(PARTITION BY branch) from datascience.marks
+
+USE datascience;
+
+SELECT * ,
+AVG(marks) OVER()  AS 'Overall_avg',
+MIN(marks) OVER(),
+MAX(marks) OVER(),
+MIN(marks) OVER(PARTITION BY branch),
+MAX(marks) OVER(PARTITION BY branch)
+FROM marks
+ORDER BY student_id
+
+
+-- Aggregate Function with over()--
+
+
+-- Find all the students who have marks higher than the avg marks of their respective branch
+
+
+SELECT * FROM (SELECT *, 
+AVG(marks) OVER(PARTITION BY branch) AS 'branch_avg'
+from datascience.marks) t
+WHERE t.marks > t.branch_avg
+
+
+
+
+
+
+
+
+
+
+
+
+
