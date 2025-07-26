474b57aeb65acd9a4901bbd23643d8a19c81f6b7
Saif Ullah <muhammadalisaif785@gmail.com>
2025-07-26 08:45:40 +0500

    Created Using MySQL Workbench
    while practicing queries on cumulative average and running average in the lecture window function in SQL

--- Lecture_07_Window_Functions_in_SQL/window_function_in_sql_queries_part_02.sql
+++ Lecture_07_Window_Functions_in_SQL/window_function_in_sql_queries_part_02.sql
@@ -17,6 +17,14 @@ GROUP BY BattingTeam,batter) t
 WHERE t.rank_within_team < 6
 ORDER BY t.BattingTeam,t.rank_within_team;
 
+-- ==================== Cumulative Average===============================
+/*
+Cumulative average is another type of average that can be calculated using
+window functions. A cumulative average calculates the average of a set of values
+up to a given point in time, and includes all previous values in the calculation.
+*/
+
+-
 
 SELECT * FROM (SELECT 
 CONCAT("Match-",CAST(ROW_NUMBER() OVER(ORDER BY ID) AS CHAR)) AS 'match_no',
@@ -28,5 +36,25 @@ FROM ipl
 WHERE batter = 'V Kohli'
 GROUP BY ID
 WINDOW w AS (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)) t
+
+-- Running Average / moving average
+
+SELECT f_name,
+(total_value/SUM(total_value) OVER())*100 AS 'percent_of_total'
+FROM (SELECT f_id,SUM(amount) AS 'total_value' FROM orders t1
+JOIN order_details t2
+ON t1.order_id = t2.order_id
+WHERE r_id = 5
+GROUP BY f_id) t
+JOIN food t3
+ON t.f_id = t3.f_id
+ORDER BY (total_value/SUM(total_value) OVER())*100 DESC
+
+SELECT YEAR(Date),QUARTER(Date),SUM(views) AS 'views',
+((SUM(views) - LAG(SUM(views)) OVER(ORDER BY YEAR(Date),QUARTER(Date)))/LAG(SUM(views)) OVER(ORDER BY YEAR(Date),QUARTER(Date)))*100 AS 'Percent_change'
+FROM youtube_views
+GROUP BY YEAR(Date),QUARTER(Date)
+ORDER BY YEAR(Date),QUARTER(Date);
+
 
 
